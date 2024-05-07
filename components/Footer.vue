7aa2eaa5a15895acbc8eced3ea086f28ce781dad
Abdullahi9 <abdullahi@nathandigital.com>
2024-05-07 12:26:35 +0300

    new space

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="w-full mt-4 ">
          <main class="max-w-sm mx-auto">
-            <h1 class="text-center text-4xl text-gray-600 rotate-90">:)</h1>
+            <h1 class="text-center text-4xl text-gray-600 rotate-90">
+               :)
+            </h1>
          </main>
     </div>
    </template>

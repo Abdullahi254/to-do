61394fad110501daf5667d0f78388a7f5262e3fd
Abdullahi9 <abdullahi@nathandigital.com>
2024-05-08 14:51:44 +0300

    new line

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,7 @@
          <main class="max-w-sm mx-auto">
             <h1 class="text-center text-4xl text-gray-600 rotate-90">
                :)
+               
             </h1>
          </main>
     </div>
